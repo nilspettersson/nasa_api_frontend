bed68592395c752ebc2780b50442eea8eee89da7
Nils Pettersson <nils-pettsson@outlook.com>
2020-09-29 20:17:21 +0200

    init

---
+++ src/components/home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    state = {  }
+    render() { 
+        return (
+            <a>home</a>
+        );
+    }
+}
+ 
+export default Home;
