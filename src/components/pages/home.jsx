615794a4fb9e284b20a41c27fe44f75d7a82e81f
Nils Pettersson <nils-pettsson@outlook.com>
2020-10-02 18:10:11 +0200

    added api call to get image of the day

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -2,15 +2,42 @@ import React, { Component } from 'react';
 import Nav from "../nav"
 
 class Home extends Component {
-    state = {  }
+    state = {
+        imgLoaded:false,
+        imgUrl:"",
+    }
+
+
+    componentDidMount(){
+        fetch("https://api.nasa.gov/planetary/apod?api_key=3lAiOi562emtnie1O6cdo43rupWcM8idQryj7g5E")
+            .then(res => res
+            .json()).then(json => {
+                console.log(json);
+                this.setState({
+                    imgLoaded: true,
+                    imgUrl: json.url
+                })
+            });
+    }
+
     render() { 
         return (
             <div>
                 <Nav page={"home"} />
-                <p>home</p>
+                {this.showImage()}
             </div>
             
         );
+    }
+
+    showImage(){
+        if(this.state.imgLoaded == true){
+            return (
+                <img src={this.state.imgUrl}/>
+            )
+        }
+        return null
+        
     }
 }
  
