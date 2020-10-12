beb92b9ae2f1fb4579a52f5dd7e5c41bc94c9d5b
Nils Pettersson <nils-pettsson@outlook.com>
2020-10-12 22:35:18 +0200

    added handleClick and handleChange to make it possible to search for images

--- src/components/pages/explore.jsx
+++ src/components/pages/explore.jsx
@@ -3,6 +3,7 @@ import Nav from "../nav"
 
 class Explore extends Component {
     state = {
+        search:"",
         loaded:false,
         images:{}
     }
@@ -20,15 +21,36 @@ class Explore extends Component {
             });
     }
 
+    handleClick = () =>{
+        fetch("https://images-api.nasa.gov/search?q=" + this.state.search + "&media_type=image")
+            .then(res => res
+            .json()).then(json => {
+                console.log(json.collection.items);
+                let images = json.collection.items;
+                this.setState({
+                    loaded: true,
+                    images:images
+                });
+            });
+    };
+    handleChange = (event) =>{
+        this.setState({search:event.target.value});
+    };
+
     render() { 
         return (
             <div>
                 <Nav page={"explore"} />
 
                 <div className="menu padding">
+                    <h3>Explore NASA images</h3>
                     <div className="search-container">
-                        <input className="search" type="text"/>
-                        <img src="%PUBLIC_URL%/../res/search.svg"/>
+                        <input 
+                            className="search" 
+                            type="text" 
+                            onChange={this.handleChange}
+                        />
+                        <img src="%PUBLIC_URL%/../res/search.svg" onClick={this.handleClick}/>
                     </div>
                 </div>
                 {this.showImages()}
@@ -43,8 +65,8 @@ class Explore extends Component {
         if(this.state.loaded){
             return(
                 <div className="grid padding">
-                    {this.state.images.map(image => 
-                    <div className="image">
+                    {this.state.images.map((image, index) => 
+                    <div key={index} className="image">
                         <img src={image.links[0].href} />
                     </div>
                     )}
