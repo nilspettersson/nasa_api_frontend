1a077106b1b0f8f60242d59888cfd3bf691f82f0
Nils pettersson <nils-pettsson@outlook.com>
2021-05-06 16:13:25 +0200

    Updates

---
+++ precache-manifest.975f0b79144e3ac91193cb359044b687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2d0d9f61ab95b706ae3d55e9c1c4eb",
+    "url": "/nasa_api_frontend/index.html"
+  },
+  {
+    "revision": "661a5eddcd9a057bfe67",
+    "url": "/nasa_api_frontend/static/css/main.60e5f7bb.chunk.css"
+  },
+  {
+    "revision": "0054686569f66c9d0692",
+    "url": "/nasa_api_frontend/static/js/2.b818118d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nasa_api_frontend/static/js/2.b818118d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661a5eddcd9a057bfe67",
+    "url": "/nasa_api_frontend/static/js/main.255b4bb3.chunk.js"
+  },
+  {
+    "revision": "0608727def585c0b89fb",
+    "url": "/nasa_api_frontend/static/js/runtime-main.90853e06.js"
+  }
+]);
