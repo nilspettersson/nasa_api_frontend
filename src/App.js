d067d873b7883bcac8b3f30e64ef3751af06b638
Nils pettersson <nils-pettsson@outlook.com>
2021-05-06 15:40:39 +0200

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 import Home from "./components/pages/home"
 import Explore from "./components/pages/explore"
