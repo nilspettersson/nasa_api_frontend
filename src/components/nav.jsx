38f09d92463bf88d6e05b5113ac00b379aa541c6
Nils pettersson <nils-pettsson@outlook.com>
2021-05-06 17:38:28 +0200

    changed to Hashrouter

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,9 +5,9 @@ class Nav extends Component {
     render() { 
         return (
             <nav>
-                <a className="title" href="/nasa_api_frontend/home">NASA EXPLORER</a>
-                <a className="link" href="/nasa_api_frontend/home">Home</a>
-                <a className="link" href="/nasa_api_frontend/explore">Explore</a>
+                <a className="title" href="/nasa_api_frontend#home">NASA EXPLORER</a>
+                <a className="link" href="/nasa_api_frontend#home">Home</a>
+                <a className="link" href="/nasa_api_frontend#explore">Explore</a>
             </nav>
         );
     }
